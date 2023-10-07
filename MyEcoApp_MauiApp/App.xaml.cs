7be998ec1826086289f565e817f134de3e49de89
Thomas Brockington <xtombrock1989@gmail.com>
2023-10-07 20:37:36 +0100

    Add project files.

---
+++ MyEcoApp_MauiApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyEcoApp_MauiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyEcoApp_MauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyEcoApp_MauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
